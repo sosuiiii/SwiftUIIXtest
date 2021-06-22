e650fbdd26da2cccf9d439bc39601469a3c50f4e
sosuiiii <wntsssbbsnws@yahoo.co.jp>
2021-06-22 14:19:21 +0900

    textField

--- SwiftUIXtest/ContentView.swift
+++ SwiftUIXtest/ContentView.swift
@@ -6,15 +6,54 @@
 //
 
 import SwiftUI
+import SwiftUIX
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+//struct ContentView: View {
+//    var body: some View {
+//        Text("Hello, world!")
+//            .padding()
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+struct ContentView: View {
+
+    @State var text: String = ""
+
+    var body: some View {
+
+        NavigationView {
+            VStack {
+                CocoaTextField("", text: $text)
+                    .isInitialFirstResponder(true)
+                    .frame(height: 50)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 15)
+                    .offset(y: 100)
+                Spacer()
+            }
+            .navigationBarTitle("New item", displayMode: .inline)
+            .navigationBarItems(leading: Button(action: {
+                print("cancelボタンタップ")
+            }, label: {
+                Text("Cancel")
+            }), trailing: Button(action: {
+                print("doneボタンタップ")
+            }, label: {
+                Text("Done")
+                    .bold()
+            }))
+        }
+    }
+
+}
+
+struct InputView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
